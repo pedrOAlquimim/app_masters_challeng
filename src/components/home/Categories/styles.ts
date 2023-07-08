a3452646b520bc761cd55c96781a32466e69dcce
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2023-07-08 15:24:54 -0300

    deploy on vercel

--- src/components/home/Categories/styles.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from '../../../../stitches.config'
-
-export const CategoriesContainer = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  flexWrap: 'wrap',
-  gap: '0.75rem',
-})
